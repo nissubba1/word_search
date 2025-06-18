f28fcdf31ab984bc724dcb2024732c8c0633a531
Nishan <116987419+nissubba1@users.noreply.github.com>
2025-06-17 20:37:14 -0400

    docs

---
+++ docs/word_search_8h.js
@@ -0,0 +1,22 @@
+var word_search_8h =
+[
+    [ "askCoordinates", "word_search_8h.html#ac23eaed35f1d529f1390f279a1c64f5a", null ],
+    [ "clearWordArray", "word_search_8h.html#ac108a8616b5d57535df1033787c1c1f8", null ],
+    [ "concatChars", "word_search_8h.html#ae95bc37adade531bf52821f6aa9dc751", null ],
+    [ "diagonalSearchDownForward", "word_search_8h.html#aaa4754b1d78d7f06bb286df76471fae7", null ],
+    [ "diagonalSearchDownReverse", "word_search_8h.html#a49cbd5e886d8fed5aeb88524ba64d477", null ],
+    [ "diagonalSearchUpForward", "word_search_8h.html#a66366660da302866051393f71a0b3bb3", null ],
+    [ "diagonalSearchUpReverse", "word_search_8h.html#a670a34b7502f713ffb45e1b2795a2db8", null ],
+    [ "displayFoundWords", "word_search_8h.html#aa91c00ac1a20ae4489c38f8a9a01ef19", null ],
+    [ "displayGrid", "word_search_8h.html#a816a0a663710b33482a8750802a038f2", null ],
+    [ "displayWordBank", "word_search_8h.html#a15ec5f7cd7bfe85c3eb85482e2f7981d", null ],
+    [ "foundWords", "word_search_8h.html#aa17dabc9ac72885bd36217113888a9c5", null ],
+    [ "horizontalSearchForward", "word_search_8h.html#ae75f2a99638259af41fc293652edb5f8", null ],
+    [ "horizontalSearchReverse", "word_search_8h.html#a36cda9066cb401351daecb899927133e", null ],
+    [ "loadFile", "word_search_8h.html#a1eea2752d20e6258d1cae9e4bf7da9f0", null ],
+    [ "printWord", "word_search_8h.html#a4ccf066781bc753339854f8177a9ea64", null ],
+    [ "validateWord", "word_search_8h.html#aaf57a1980a1dca4154e323610c7330a0", null ],
+    [ "verticalSearchForward", "word_search_8h.html#a7e699ea54493bbf3d922b56582b1f215", null ],
+    [ "verticalSearchReverse", "word_search_8h.html#a4e3dbb8b49ab5a383c9f68a65725a315", null ],
+    [ "GRID_SIZE", "word_search_8h.html#a412aaef61a623a4acb699ef4099b2491", null ]
+];
